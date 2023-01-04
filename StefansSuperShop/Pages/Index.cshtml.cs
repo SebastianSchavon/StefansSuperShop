55aa41970ee3a289d1d4a0c8d5eaaaec9f5cfc5d
Frida <fridaawesterdahl@gmail.com>
2023-01-04 13:32:03 +0100

    Displayar kategorier

--- StefansSuperShop/Pages/Index.cshtml.cs
+++ StefansSuperShop/Pages/Index.cshtml.cs
@@ -15,14 +15,19 @@ namespace StefansSuperShop.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly ApplicationDbContext _context;
 
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
+        public List<TrendingCategory> TrendingCategories { get; set; }
         public class TrendingCategory
         {
             public int Id { get; set; }
             public string Name { get; set; }
         }
-        public List<TrendingCategory> TrendingCategories { get; set; }
-
+        
         public List<Product> NewProducts { get; set; }
         public class Product
         {
@@ -31,24 +36,18 @@ namespace StefansSuperShop.Pages
             public decimal? Price { get; set; }
         }
 
-
-
-        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
+        public List<Category> Categories { get; set; }
+        public class Category
         {
-            _logger = logger;
-            _context = context;
+            public int Id { get; set; }
+            public string Name { get; set; }
         }
 
         public void OnGet()
         {
             TrendingCategories = _context.Categories.Take(3).Select(c =>
-                new TrendingCategory { Id = c.CategoryId, Name = c.CategoryName }
-            ).ToList();
-        }
-
-        public ApplicationDbContext Get_context()
-        {
-            return _context;
+                new TrendingCategory { Id = c.CategoryId, Name = c.CategoryName })
+                .ToList();
         }
 
         public List<Product> GetNewProducts()
@@ -56,7 +55,17 @@ namespace StefansSuperShop.Pages
             NewProducts = _context.Products.OrderByDescending(p => p.ProductId).Take(10)
                 .Select(p => new Product { Id = p.ProductId, Name = p.ProductName, Price = p.UnitPrice})
                 .ToList();
+
             return NewProducts;
+        }
+
+        public List<Category> GetCategories()
+        {
+            Categories = _context.Categories.Select(c => 
+                new Category { Id = c.CategoryId, Name = c.CategoryName })
+                .ToList();
+
+            return Categories;
         }
     }
 }
